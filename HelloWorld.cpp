ffa7c43d2140928919027d5ea09426ce08f565ba
William B <williamb19084@gmail.com>
2025-10-27 10:48:37 -0400

    Added the std namespace

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-	cout << "Hello teach. My name is William and I'm in your CMSC 313 class\n";
+	cout << "Hello teach. My name is William and I'm in your CMSC 313 class.\n";
 	return 0;
 } // End of main
